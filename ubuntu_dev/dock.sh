596be3f9c81e2ba11b090f522eb3bd0af97021ef
Yuxiang Gu <lucas@ventureum.io>
2019-03-12 05:11:42 -0400

    minor change in dock.sh

--- ubuntu_dev/dock.sh
+++ ubuntu_dev/dock.sh
@@ -1,4 +1,4 @@
 ##!/bin/bash
 # sudo xhost +
-# docker run --rm -it --shm-size=1gb -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached $(docker build -q .)
-docker run --rm -it --shm-size=1gb -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached yuxianggu/ubuntu_dev:latest
+# docker run --rm -it --shm-size=1gb --name ubuntu -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached $(docker build -q .) 
+docker run --rm -it --shm-size=1gb --name ubuntu -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached yuxianggu/ubuntu_dev:latest
