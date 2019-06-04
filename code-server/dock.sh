c0b67ee38d38bba69ce3ef4560d7b3c6e980de57
Yuxiang Gu <lucas@ventureum.io>
2019-06-04 03:13:28 -0400

    change crlf to lf

--- code-server/dock.sh
+++ code-server/dock.sh
@@ -1,1 +1,3 @@
-docker run --rm -it --shm-size=2gb --name code-server -p 127.0.0.1:8443:8443 -v c:/Users/yuxia/docker-volumes/code-server:/root:cached yuxianggu/code-server:latest
+docker run --rm -it --shm-size=2gb --name code-server -p 127.0.0.1:8443:8443 -v c:/Users/yuxia/docker_volumes/code-server:/root:cached yuxianggu/code-server:latest
+docker build -t code-serve c:/Users/yuxia/projects/dockerfiles/code-server
+docker run --rm -it --shm-size=2gb --name code-server -p 127.0.0.1:8443:8443 -v c:/Users/yuxia/docker_volumes/code-server:/root:cached code-server:latest
--- ubuntu_dev/dock.sh
+++ ubuntu_dev/dock.sh
@@ -1,4 +1,3 @@
 ##!/bin/bash
 # sudo xhost +
-# docker run --rm -it --shm-size=1gb --name ubuntu -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached $(docker build -q .) 
-docker run --rm -it --shm-size=1gb --name ubuntu -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached yuxianggu/ubuntu_dev:latest
+# docker run --rm -it --shm-size=1gb --name ubuntu -v c:/Users/yuxia/docker_ubuntu_dev:/home/devg:cached $(do
